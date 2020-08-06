6221be735c6d79607e3cc34a8eac49d1d9d2504f
Kirill Andreev <Hizenberg228@,ail.ru>
2020-08-06 13:39:28 +0300

    add tags in post (Many to many)

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True) #db_index - делает индекс каждому полю
     slug = models.SlugField(max_length=150, unique=True)    #unique - делает каждое поле уникальным
     body = models.TextField(blank=True, db_index=True)      #blank=True - позволяет оставлять поле пустым
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts') #related_name - для связи
+                                                                            # с классом Tag будет использоваться 'posts'
     date_pub = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
@@ -18,3 +20,15 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return f'{self.title}'
+
